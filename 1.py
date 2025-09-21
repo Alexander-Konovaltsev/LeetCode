f2981e7a3aa59ebbb3bbe3b2bf1f1eba4c24cd16
Alexander-Konovaltsev <aleksandar.konovaltseff@yandex.ru>
2025-09-21 21:53:28 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,19 +2,21 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for idx_1, num_1 in enumerate(nums):
-            for idx_2, num_2 in enumerate(nums):
+        nums_idxes = {}
 
-                if idx_1 == idx_2:
-                    continue
-                if (num_1 + num_2) == target:
-                    return [idx_1, idx_2]
-                        
-        return [0]
+        for idx, num in enumerate(nums):
+            diff = target - num
+
+            if diff in nums_idxes:
+                return [nums_idxes[diff], idx]
+            
+            nums_idxes[num] = idx
+            
+        return []
     
 sol = Solution()
 
-test_cases = [ [[2, 7, 11, 15], 9], [[3, 2, 4], 6], [[3, 3], 6]]
+test_cases = [[[2, 7, 11, 15], 9], [[3, 2, 4], 6], [[3, 3], 6]]
 
 for case in test_cases:
     print(sol.twoSum(case[0], case[1]))
