39c46858802f18765be096f01366efe98c393235
Alexander-Konovaltsev <aleksandar.konovaltseff@yandex.ru>
2025-09-22 08:47:00 +0300

    Add 3005.py

---
+++ 3005.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        nums_count = {}
+        for num in nums:
+            nums_count[num] = nums_count.get(num, 0) + 1
+
+        max_frequency = max(list(nums_count.values()))
+        nums_with_max_frequency = 0
+        
+        for val in nums_count.values():
+            if val == max_frequency:
+                nums_with_max_frequency += 1
+
+        return nums_with_max_frequency * max_frequency
+    
+sol = Solution() 
+
+test_cases = [[1,2,2,3,1,4], [1,2,3,4,5]]
+
+for case in test_cases:
+    print(sol.maxFrequencyElements(case))
